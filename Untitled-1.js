bf90ebffeeb87d3eb6f57ddb56cead02e948f5e8
FlorenceL453 <90851771+FlorenceL453@users.noreply.github.com>
2021-10-07 14:40:16 -0400

    image

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,7 +2,7 @@ const chapterObjet = {
   chapter1: {
     subtitle: "Lépave",
     text: "Vous arriver à un épave de bateau d'un chasseur de trésor. Est-ce que vous sauter à l'eau pour voler ces indices ou vous chercher par vous même tout vos indices?",
-    img: "thumb_720_450_dreamstime_xxl_10604660-Custom.jpg",
+    img: "../assets/img/thumb_720_450_dreamstime_xxl_10604660-Custom.jpg",
     option: [
       {
         text: "Je veux tout chercher mes indices par moi même",
@@ -17,7 +17,7 @@ const chapterObjet = {
   chapter2: {
     subtitle: "L'indice",
     text: "Une rumeur circule dans la ville qui raconte que le trésors serait en haute mer. Est-ce que vous voulez la suivre ou vous fier à votre indice",
-    img: "5993790.jpg",
+    img: "../assets/img/5993790.jpg",
     option: [
       {
         text: "On suit la rumeur elle fait du sens",
@@ -32,7 +32,7 @@ const chapterObjet = {
   chapter3: {
     subtitle: "Le fort",
     text: "Est-ce que vous décider de croire et de suivre les indication du veille homme pour trouver le trésor ou vous penser qu'il veux seulement vous en éloigne?",
-    img: "Outerbanks show.jpg",
+    img: "../assets/img/Outerbanks show.jpg",
     option: [
       {
         text: "On ne suis pas ces conseilles il veux surement nous éloigner du trésor",
@@ -47,7 +47,7 @@ const chapterObjet = {
   chapter4: {
     subtitle: "L'emplacement du trésor",
     text: "Alors rendu à cette étape une carte de l'île est nécéssaire pour savoir ou est l'emplacement que vous avez trouver sur l'île alors l'avez-vous?",
-    img: "parcel-map-final-1589481133.jpg",
+    img: "../assets/img/parcel-map-final-1589481133.jpg",
     option: [
       {
         text: "Non je n'est pas la carte de l'île",
@@ -73,8 +73,9 @@ function goToChapter(chapterName) {
   sT.innerText = chapter.text;
   console.log(sT.innerText);
 
-  const img = document.querySelector(".image1");
-  img.innerHTML =
-    "<img src =\\assets img \thumb_720_450_dreamstime_xxl_10604660-Custom.jpg>";
-  console.log(img.innerHTML);
+  const image = document.querySelector(".image1");
+  image.src = chapter.img;
+  console.log(image.innerHTML);
+
+  // console.log(option.innerText);
 }
