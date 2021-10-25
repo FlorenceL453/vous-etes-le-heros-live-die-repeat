117543b6646a448057218a8bd37c1fd5b2e5fc98
smnarnold <moi@smnarnold.com>
2021-10-24 20:50:05 -0400

    Fixes

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,122 +2,122 @@ const chapterObjet = {
   chapterReveille: {
     subtitle: "Le Début De La Journée",
     text: "Votre personnage Jonh B. se réveille",
-    img: "../assets/img/johnb.jpg",
+    img: "assets/img/johnb.jpg",
     option: [
       {
         text: "Je commence ma journée",
-        action: "goToChapter(chapter1)",
+        action: "goToChapter('chapter1')",
       },
     ],
   },
   chapter1: {
     subtitle: "Lépave",
     text: "Vous arriver à un épave de bateau d'un chasseur de trésor. Est-ce que vous sauter à l'eau pour voler ces indices ou vous chercher par vous même tout vos indices?",
-    img: "../assets/img/thumb_720_450_dreamstime_xxl_10604660-Custom.jpg",
+    img: "assets/img/thumb_720_450_dreamstime_xxl_10604660-Custom.jpg",
     option: [
       {
         text: "Je veux tout chercher mes indices par moi même",
-        action: "goToChapter(chapter1MC)",
+        action: "goToChapter('chapter1MC')",
       },
       {
         text: "On pourrais bien bénéficier d'aide pour commence",
-        action: "goToChapter(chapter2)",
+        action: "goToChapter('chapter2')",
       },
     ],
   },
   chapter1MC: {
     subtitle: "Vous manquer un indice",
     text: "En cherchant par vous même vous manquer un indice important dons vous devez recommencer votre journée pour avoir une chance de la trouver.",
-    img: "../assets/img/outerbanks_boat.jpg",
+    img: "assets/img/outerbanks_boat.jpg",
     option: [
       {
         text: "Je veux tout chercher mes indices par moi même",
-        action: "goToChapter(chapterReveille)",
+        action: "goToChapter('chapterReveille')",
       },
     ],
   },
   chapter2: {
     subtitle: "La Bousole",
     text: "Vous trouver une bousole avec un indice écrit à l'arrière. ",
-    img: "../assets/img/bousole.jpg",
+    img: "assets/img/bousole.jpg",
     option: [
       {
         text: "Je lis l'indice",
-        action: "goToChapter(chapitre3)",
+        action: "goToChapter('chapitre3')",
       },
     ],
   },
   chapter3: {
     subtitle: "L'indice",
     text: "Une rumeur circule dans la ville qui raconte que le trésors serait en haute mer. Est-ce que vous voulez la suivre ou vous voulez vous fier à votre indice",
-    img: "../assets/img/5993790.jpg",
+    img: "assets/img/5993790.jpg",
     option: [
       {
         text: "On suit la rumeur elle fait du sens",
-        action: "goToChapter(chapter3MC)",
+        action: "foundedmap()",
       },
       {
         text: "Je suis mon indice, il faut pas ce faire distraire par une faisse rumeur",
-        action: "goToChapter(chapter4)",
+        action: "goToChapter('chapter4')",
       },
     ],
   },
   chapter3MC: {
     subtitle: "La rumeur",
     text: "En se préparant pour aller en haute mer vous trouver une carte de l'île dans le bureau de votre père. Mais vous ne trouver pas de trésor en haute mers.",
-    img: "../assets/img/rumeur.png",
+    img: "assets/img/rumeur.png",
     option: [
       {
         text: "Je veux tout chercher mes indices par moi même",
-        action: "goToChapter(chapter6)",
+        action: "goToChapter('chapter6')",
       },
     ],
   },
   chapter4: {
     subtitle: "Le Chasseur de Trésor",
     text: "En suivant l'indice cela vous mène à un ancien chasseur de trésor qui vous dit que le trésor n'est pas dans un épave de bateau mais bien sur l'île",
-    img: "../assets/img/mauvaise_desicion.jpg",
+    img: "assets/img/mauvaise_desicion.jpg",
     option: [
       {
         text: "Quoi faire ?",
-        action: "goToChapter(chapitre5)",
+        action: "goToChapter('chapitre5')",
       },
     ],
   },
   chapter5: {
     subtitle: "Le fort",
     text: "Est-ce que vous décider de croire et de suivre les indication du veille homme pour trouver le trésor ou vous penser qu'il veux seulement vous en éloigne?",
-    img: "../assets/img/Outerbanks show.jpg",
+    img: "assets/img/Outerbanks show.jpg",
     option: [
       {
         text: "On ne suis pas ces conseilles il veux surement nous éloigner du trésor",
-        action: "goToChapter(chapter5MC)",
+        action: "goToChapter('chapter5MC')",
       },
       {
         text: "On vas suivre ses conseille, il à l'air de connaitre mieu le trésor que nous et il existe des personne bien intentionné dans la vie",
-        action: "goToChapter(chapter6)",
+        action: "goToChapter('chapter6')",
       },
     ],
   },
   chapter5MC: {
     subtitle: "Une mauvaise désicion",
     text: "En ne suivant pas ses conseilles vous rater une piste très importante a votre quête donc vous devez recommencer votre journée pour avoir une chance de trouver le trésor",
-    img: "../assets/img/chasseur.jpg",
+    img: "assets/img/chasseur.jpg",
     option: [
       {
         text: "Ok je vais recommencer",
-        action: "goToChapter(chapterReveille)",
+        action: "goToChapter('chapterReveille')",
       },
     ],
   },
   chapter6: {
     subtitle: "Vous comprener ou est cahcer le trésor sur l'île.",
     text: "En suivant la piste du chasseur de trésor vous trouver l'emplacement du trésor ",
-    img: "../assets/img/chapter6.jpg",
+    img: "assets/img/chapter6.jpg",
     option: [
       {
         text: "yey!!! Un indice de plus",
-        action: "goToChapter(chapter7)",
+        action: "map()",
       },
     ],
   },
@@ -125,40 +125,40 @@ const chapterObjet = {
   chapter7: {
     subtitle: "L'emplacement du trésor",
     text: "Alors rendu à cette étape une carte de l'île est nécéssaire pour savoir ou est l'emplacement que vous avez trouver sur l'île alors l'avez-vous?",
-    img: "../assets/img/parcel-map-final-1589481133.jpg",
+    img: "assets/img/parcel-map-final-1589481133.jpg",
     option: [
       {
         text: "Non je n'est pas la carte de l'île",
-        action: "goToChapter(chapterReveille)",
+        action: "goToChapter('chapterReveille')",
       },
       {
         text: "Oui j'ai la carte de l'île",
-        action: "goToChapter(chapter7)",
+        action: "goToChapter('chapter7')",
       },
     ],
   },
   chapter7MC: {
     subtitle: "La carte de l'île",
     text: "Parce que vous avez pas la carte de l'île vous dever recommencer pour la trouver.",
-    img: "../assets/img/pas_carte.jpg",
+    img: "assets/img/pas_carte.jpg",
     option: [
       {
         text: "D'accord",
-        action: "goToChapter(chapterReveille)",
+        action: "goToChapter('chapterReveille')",
       },
     ],
   },
   chapter8: {
     subtitle: "Le trésor",
     text: "Alors avec votre carte vous arrivez  à l'emplacement du trésor. Vous trouver le trésor intact sous le sol. Donc vous prenez toute l'or qu'il s'y trouve et vous la vendez pour récolter l'argent et vous devenez vraiment riche.",
-    img: "../assets/img/parcel-map-final-1589481133.jpg",
+    img: "assets/img/parcel-map-final-1589481133.jpg",
+    option: []
   },
 };
 function goToChapter(chapterName) {
-  foundedmap("chapter3MC");
+  // foundedmap("chapter3MC");
   let chapter = chapterObjet[chapterName];
-  // console.log(chapter.subtitle);
-  //  console.log(chapter.text);
+
   const ti = document.querySelector(".titre");
   ti.innerText = chapter.subtitle;
   console.log(ti.innerText);
@@ -171,37 +171,21 @@ function goToChapter(chapterName) {
   image.src = chapter.img;
   console.log(image.innerHTML);
 
-  for (let index = 0; index < 1; index++) {
-    console.log(chapter.option.text);
-
-    const tab1 = document.querySelector(".bouton1");
-    tab1.innerHTML = chapter.option[index].text;
-
-    console.log(tab1.innerHTML);
-
-    //boutons chapitre
-
-    document.querySelector(
-      "barreoption"
-    ).innerHTML = `<div><button type="button" value= '${chapterObjet[chapterName].option[index].text}' onclick=' ${chapterObjet[chapterName].option[index].action}'/></div>`;
-
-    //const click1 = document.querySelector(".bouton1");
-    //click1.onclick = function () {};
+  const barreOptions = document.querySelector(".barreoption");
+  let buttons = '';
+  for (let index = 0; index < chapter.option.length; index++) {
+    buttons += `<button onclick="${chapter.option[index].action}"/>${chapter.option[index].text}</button>`;
   }
-
-  //for (let index = 0; index < 2; index++) {
-   // console.log(chapter.option.text);
-
-    //const tab2 = document.querySelector(".bouton2");
-    //tab2.innerHTML = chapter.option[index].text;
-    //console.log(tab2.innerHTML);
-  //}
+  barreOptions.innerHTML = buttons;
 }
+
 let mapFounded = false;
 
 function foundedmap() {
   mapFounded = true;
+  goToChapter('chapter3MC')
 }
+
 function map() {
   if (mapFounded == true) {
     goToChapter("chapter8");
@@ -210,3 +194,5 @@ function map() {
     goToChapter("chapter7MC");
   }
 }
+
+goToChapter('chapterReveille');
