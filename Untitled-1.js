fb49028142cd3dd3a0ba5181987d36975c168101
FlorenceL453 <90851771+FlorenceL453@users.noreply.github.com>
2021-11-08 14:34:35 -0500

    vid

--- Untitled-1.js
+++ Untitled-1.js
@@ -181,6 +181,7 @@ function goToChapter(chapterName) {
   barreOptions.innerHTML = buttons;
   if (chapter == video) {
     `<video src="assets/vid/giphy1.mp4"></video>`;
+    console.log(chapter.video);
   }
 }
 
